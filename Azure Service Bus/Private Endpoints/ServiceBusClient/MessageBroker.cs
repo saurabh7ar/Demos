efa91eb9cd1d0d26a902806951612a96638a36ac
Saurabh Agarwal <sauraagarwal@microsoft.com>
2023-06-19 16:16:01 -0500

    Added Service Bus Client Project

---
+++ Azure Service Bus/Private Endpoints/ServiceBusClient/MessageBroker.cs
@@ -0,0 +1,29 @@
+﻿using Azure.Messaging.ServiceBus;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client
+{
+    public class MessageBroker
+    {
+        private readonly string _connectionString;
+        private readonly string _queueName;
+
+        public MessageBroker(string connectionString, string queueName)
+        {
+            _connectionString = connectionString;
+            _queueName = queueName;
+        }
+
+        public async Task SendMessageAsync(string messageBody)
+        {
+            await using var client = new ServiceBusClient(_connectionString);
+            var sender = client.CreateSender(_queueName);
+            var message = new ServiceBusMessage(messageBody);
+            await sender.SendMessageAsync(message);
+        }
+    }
+}
